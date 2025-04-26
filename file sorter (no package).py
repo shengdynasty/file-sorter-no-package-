f3497b4caef1b7139e4d79fb83365f3876bd9cd2
shengdynasty <149344545+shengdynasty@users.noreply.github.com>
2025-04-26 18:54:14 -0500

    Delete file sorter (no package).py

--- file sorter (no package).py
+++
@@ -1,8 +0,0 @@
-import os 
-def move_file(source, destination):
-    # Move by renaming (works inside same drive/disk)
-    os.rename(source,destination)
-
-def sort_files_by_estension(source_folder):
-    if not os.path.isdir(source_folder):
-        print(f"The folder {source_folder} does not exist.")
